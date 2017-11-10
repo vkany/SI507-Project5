51306e6bce5f58f63222027c93e24f2f2f796d91
Vibhuti Kanitkar <vibhuti@umich.edu>
2017-11-10 00:46:31 -0500

    added cached data

--- SI507project5_code.py
+++ SI507project5_code.py
@@ -1,4 +1,5 @@
-## import statements
+## import statements:
+
 from requests_oauthlib import OAuth2Session
 from secret_data import app_id , app_secret, personal_token
 import webbrowser
@@ -6,7 +7,6 @@ import json
 from datetime import datetime
 
 ## CACHING SETUP
-
 APP_ID = app_id
 APP_SECRET = app_secret
 AUTHORIZATION_BASE_URL = 'https://www.eventbrite.com/oauth/authorize'
@@ -14,44 +14,64 @@ TOKEN_URL = 'https://www.eventbrite.com/oauth/token'
 REDIRECT_URI = 'https://www.programsinformationpeople.org/runestone/oauth'
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 eb_session = False
+CACHE_DICTION = {}
+CACHE_FNAME = 'cached_event_list.json'
 
-## ADDITIONAL CODE for program should go here...
-## Perhaps authentication setup, functions to get and process data, a class definition... etc.
-# q = rawinput("What event would you like to attend?")
-# if q = "" :
-#     q = "Art Show"
-# else:
-#      q = q
+# caching
+def load_cache():
+    global CACHE_DICTION
+    try:
+        cache_file = open(CACHE_FNAME, 'r')
+        cache_contents = cache_file.read()
+        CACHE_DICTION = json.loads(cache_contents)
+        cache_file.close()
+    except:
+        CACHE_DICTION = {}
 
-# Set up sessions and so on to get data via OAuth2 protocol...
-#
-# oauth2inst = OAuth2Session(APP_ID, redirect_uri=REDIRECT_URI) # Create an instance of an OAuth2Session
-#
-# authorization_url, state = oauth2inst.authorization_url(AUTHORIZATION_BASE_URL) # all we need for spotify
-#
-# webbrowser.open(authorization_url) # Opening auth URL for you to sign in to the Spotify service
-# authorization_response = input('Authenticate and then enter the full callback URL: ').strip() # Need to get the full URL in order to parse the response
-#
-# # The OAuth2Session instance has a method that extracts what we need from the url, and helps do some other back and forth with spotify
-# token = oauth2inst.fetch_token(TOKEN_URL, authorization_response=authorization_response, client_secret=APP_SECRET)
-## On a web server, this would happen on your server, but we have to pull the token out so we can use it for a request inside a script we're running on a personal computer (with connection to internet)
-## Anytime we want to get new data we have to do this -- so a caching system would have to take this into account any time the data expired.
-## And for that -- we'd want to think about the API rate limits, primarily!
-# print(token)
+def save_cache():
+    full_text = json.dumps(CACHE_DICTION)
+    cache_file_ref = open(CACHE_FNAME,"w")
+    cache_file_ref.write(full_text)
+    cache_file_ref.close()
+
+def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
+    alphabetized_keys = sorted(params_d.keys())
+    res = []
+    for k in alphabetized_keys:
+        if k not in private_keys:
+            res.append("{}-{}".format(k, params_d[k]))
+    return baseurl + "_".join(res)
+
 def make_eb_request(url, params=None):
     # we use 'global' to tell python that we will be modifying this global variable
     global eb_session
 
-    if not eb_session:
-        start_eb_session()
-
     if not params:
         params = {}
 
-    return eb_session.get(url, params=params)
+    unique_ident = params_unique_combination(url, params)
+
+    # if not in cache, get fresh data
+    if unique_ident not in CACHE_DICTION:
+        if not eb_session:
+            start_eb_session()
+
+        response = eb_session.get(url, params=params)
+        # response = requests.get(baseurl, params=params)
+        CACHE_DICTION[unique_ident] = json.loads(response.text)
+        save_cache()
+        print('--> fresh copy')
+    else:
+        print('--> cached')
+
+    return CACHE_DICTION[unique_ident]
+
+    # return eb_session.get(url, params=params)
+
 
 def start_eb_session():
     global eb_session
+
 
     # 0 - get token from cache
     try:
@@ -83,7 +103,6 @@ def get_saved_token():
     with open('token.json', 'r') as eb:
         token_json = eb.read()
         token_dict = json.loads(token_json)
-
         return token_dict
 
 def save_token(token_dict):
@@ -91,13 +110,24 @@ def save_token(token_dict):
         token_json = json.dumps(token_dict)
         f.write(token_json)
 
+load_cache()
+
 # Now we can just use the get method on the oauth2session instance from here on out to make requests to spotify endpoints. Token will work for any endpoint, as long as it's still valid. (How long it is will depend from API to API)
 API_BASE_URL = 'https://www.eventbriteapi.com/v3'
-r = make_eb_request(API_BASE_URL + '/events/search/',params={
-    'q': 'Art Show',
-    'location.address': 'Detroit, MI'
-})
-response_diction = json.loads(r.text)
-print(json.dumps(response_diction, indent=2))
-# print(json.dumps(response_diction, indent=2))
-## Make sure to run your code and write CSV files by the end of the program.
+def get_response_diction(query,location):
+    return make_eb_request(API_BASE_URL + '/events/search/',params={
+    'q': query,
+    'location.address': location,
+    'sort_by':'-date',
+    'start_date.keyword':'this_week'
+    })
+
+
+# response_diction = json.loads(r.text)
+print(json.dumps(get_response_diction('Art Show','New York'), indent=2))
+print(json.dumps(get_response_diction('Music Concerts','Detroit'), indent=2))
+# with open("Detroit_art_events.csv", 'w', newline='') as ark:
+#     writer = csv.writer(ark)
+#     ark.write("Name,Location,Type,Address,Description\n")
+#     for obj in arkansas_natl_sites:
+#         writer.writerow([obj.name, obj.location, obj.type, obj.get_mailing_address(), obj.description])
